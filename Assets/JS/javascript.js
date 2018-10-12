c334ae8da8362afa76c835a2f6d058b9d801bda2
Boivin <boivinhelene@yahoo.com>
2018-10-12 14:00:07 +0200

    JS Partie 1

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,12 @@
+function Résultat(){
+var age = document.getElementById("age").value;
+if(isNaN(age) || age < 1){
+  alert("Entrer des chiffres");
+}else{
+if
+  (age >= 18){
+  alert("Vous êtes majeur");
+}else{
+alert("Vous êtres mineur");}
+}
+}
